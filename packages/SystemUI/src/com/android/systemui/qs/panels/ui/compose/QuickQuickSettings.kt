dc4f98d42b0cec0f4d8841dc8702c5d19618407a
Dmitrii <bankersenator@gmail.com>
2026-02-28 17:58:07 +0000

    SystemUI: shade perf improvements
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/QuickQuickSettings.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/QuickQuickSettings.kt
@@ -100,6 +100,9 @@ fun ContentScope.QuickQuickSettings(
     }
     val classicStyle = rememberQSPanelStyle()
 
+    val bounceables = remember(sizedTiles) { List(sizedTiles.size) { BounceableTileViewModel() } }
+    val spans by remember(sizedTiles) { derivedStateOf { sizedTiles.fastMap { it.width } } }
+
     Box(modifier = modifier) {
         GridAnchor()
 
@@ -128,10 +131,6 @@ fun ContentScope.QuickQuickSettings(
                 )
             }
         } else {
-            val bounceables =
-                remember(sizedTiles) { List(sizedTiles.size) { BounceableTileViewModel() } }
-            val spans by remember(sizedTiles) { derivedStateOf { sizedTiles.fastMap { it.width } } }
-            
             VerticalSpannedGrid(
                 columns = columns,
                 columnSpacing = if (useModifiedSpacing) {
